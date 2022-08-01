b479737b3363378355b2d624db0f47d07e9f499c
Johnny Oak <joao.m.carvalho@outlook.pt>
2022-08-01 15:39:29 +0100

    Adding redis

--- helpers/quotes.ts
+++ helpers/quotes.ts
@@ -1,7 +1,27 @@
 import { readJsonSync } from 'fs-extra';
+import { getValue } from './redis';
 import { getRandom } from './utils';
 
 const getQuotes = (): string[] => readJsonSync(`${__dirname}/configurations/quotes.json`);
+
+const getQuotes2 = async (): Promise<string[]> => {
+    const quotes = await getValue('quotes');
+
+    if (!quotes) return [];
+
+    return JSON.parse(quotes);
+};
+
+const getQuote2 = async () => {
+    try {
+        const quotes = await getQuotes2();
+        const index = getRandom(quotes.length);
+
+        return quotes[index - 1];
+    } catch (error) {
+        throw error;
+    }
+};
 
 export const getQuote = () => {
     const quotes = getQuotes();
