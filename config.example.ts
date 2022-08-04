358357303233679198b9a2c56f3a49c698c2945e
Johnny Oak <joao.m.carvalho@outlook.pt>
2022-08-04 09:57:52 +0100

    Added an example config file

---
+++ config.example.ts
@@ -0,0 +1,12 @@
+// THIS IS AN EXAMPLE FILE
+/**
+ * THIS IS AN EXAMPLE CONFIGURATION FILE
+ *
+ * How to use:
+ * - Fill up the constants with the correct values.
+ * - Rename the file to config.ts
+ */
+
+export const TOKEN = '[INSERT YOUR TOKEN HERE]';
+export const PREFIX = '!';
+export const REACTION_MAX = 3;
