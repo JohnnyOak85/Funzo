0f4a1b75366ddb71bb21fe9f13d4fc1873789f9d
Johnny Oak <joao.m.carvalho@outlook.pt>
2022-08-09 19:27:46 +0100

    Updated bday

--- helpers/bday.ts
+++ helpers/bday.ts
@@ -1,8 +1,5 @@
 import { appendMap } from './redis';
-
-const DATE_REGEX = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/gm;
-
-const isValidDate = (date: string) => date.length === 4 || DATE_REGEX.test(date);
+import { isValidDate } from './utils';
 
 export const recordBirthday = (id = '', date = '') => {
     try {
@@ -10,13 +7,11 @@ export const recordBirthday = (id = '', date = '') => {
             return 'User is invalid';
         }
 
-        date = `${new Date().getFullYear()}-${date}`;
-
         if (!isValidDate(date)) {
             return 'Date is invalid';
         }
 
-        appendMap('bdays', { [id]: new Date(date).toISOString() });
+        appendMap('bdays', { [id]: date });
 
         return 'Birthday has been recorded';
     } catch (error) {
